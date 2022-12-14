1b62ddaee428e39036a0a9b42e9d24180cf7a921
KovalMaxim1989 <110034072+KovalMaxim1989@users.noreply.github.com>
2022-12-14 13:32:14 +0200

    markUpEl
    markUpEl

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,18 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery')
+
+const markUp = galleryItems.map(({ preview, original, description }) => `
+<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`).join('');
+galleryEl.innerHTML = markUp;
+console.log(markUp);
