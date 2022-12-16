1c2fe25f4a40095cfb746dc1b2ca5f5d057baaa9
Maxim Koval <110034072+KovalMaxim1989@users.noreply.github.com>
2022-12-16 14:22:18 +0200

    task2
    task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery')
+
+const markUp = galleryItems
+.map(({ preview, original, description }) =>
+`
+ <li>
+   <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+ </li>
+`)
+.join('');
+
+galleryEl.innerHTML = markUp;
+// console.log(SimpleLightbox);
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+
+    
